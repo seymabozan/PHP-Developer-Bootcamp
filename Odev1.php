e93f87c4600f6fc67d5947dbd7cc191f998da016
Şeyma Bozan <101289239+seymabozan@users.noreply.github.com>
2022-11-05 19:27:20 +0300

    Update Odev1.php

--- Odev1.php
+++ Odev1.php
@@ -1,66 +1,69 @@
 <?php
 
 $index = array(
-    array("agil" => 5, "kapasite" => 150, "koyun" => 73),
-    array("agil" => 4, "kapasite" => 160, "koyun" => 160),
-    array("agil" => 3, "kapasite" => 135, "koyun" => 147)
+    array("agil" => 5, "agilkapasite" => 30, "kapasite" => 150, "koyun" => 73),
+    array("agil" => 4, "agilkapasite" => 40, "kapasite" => 160, "koyun" => 160),
+    array("agil" => 3, "agilkapasite" => 35, "kapasite" => 105, "koyun" => 145)
 );
 
 echo "Toplam Ağıl: ".$index[0]["agil"]."<br>";
+echo "Ağıl Kapasitesi: ".$index[0]["agilkapasite"]."<br>";
 echo "Toplam Kapasite: ".$index[0]["kapasite"]."<br>";
 echo "Toplam Koyun: ".$index[0]["koyun"]."<br><br>";
 
-$agil = array("1. Agil" => 0, "2. Agil" => 0, "3. Agil" => 13, "4. Agil" => 30, "5. Agil" => 30);
-$tersagil = array_reverse($agil);
-if($index[0]["agil"]==5 && $index[0]["kapasite"]==150){
-    foreach($tersagil as $agilsayisi => $kapasitesi){
-        echo $agilsayisi.": ".$kapasitesi."<br>";
+for($i=$index[0]["agil"]; $i>=1; $i--){
+    if($index[0]["koyun"] >= $index[0]["agilkapasite"]){
+    echo "$i. Agil: ".$index[0]["agilkapasite"]."<br>";
     }
-}
-else{
-    echo "ÇIKTI HATASI";
-}
+    else{
+        if($index[0]["koyun"] >= 0){
+            echo "$i. Agil: ".$index[0]["koyun"]."<br>";
+        }
+        else{
+            echo "$i. Agil: 0"."<br>";   
+        }
+    }
+    $index[0]["koyun"] = $index[0]["koyun"] - $index[0]["agilkapasite"];
+};
 
-echo "<hr>";
+echo "<br><hr><br>";
 
 echo "Toplam Ağıl: ".$index[1]["agil"]."<br>";
+echo "Ağıl Kapasitesi: ".$index[1]["agilkapasite"]."<br>";
 echo "Toplam Kapasite: ".$index[1]["kapasite"]."<br>";
 echo "Toplam Koyun: ".$index[1]["koyun"]."<br><br>";
 
-$agil2 = array("1. Agil" => 40, "2. Agil" => 40, "3. Agil" => 40, "4. Agil" => 40);
-$tersagil2 = array_reverse($agil2);
-if($index[1]["agil"]==4 && $index[1]["kapasite"]==160){
-    foreach($tersagil2 as $agilsayisi2 => $kapasitesi2){
-        echo $agilsayisi2.": ".$kapasitesi2."<br>";
+for($i=$index[1]["agil"]; $i>=1; $i--){
+    if($index[1]["koyun"] >= $index[1]["agilkapasite"]){
+    echo "$i. Agil: ".$index[1]["agilkapasite"]."<br>";
     }
-}
-else{
-    echo "ÇIKTI HATASI";
-}
+};
 
-echo "<hr>";
+echo "<br><hr><br>";
 
 echo "Toplam Ağıl: ".$index[2]["agil"]."<br>";
+echo "Ağıl Kapasitesi: ".$index[2]["agilkapasite"]."<br>";
 echo "Toplam Kapasite: ".$index[2]["kapasite"]."<br>";
 echo "Toplam Koyun: ".$index[2]["koyun"]."<br><br>";
-$disarida_kalan= $index[2]["koyun"] - $index[2]["kapasite"]." Koyun";
 
-$agil3 = array("1. Agil" => 45, "2. Agil" => 45, "3. Agil" => 45);
-$tersagil3 = array_reverse($agil3);
-if($index[2]["agil"]==3 && $index[2]["kapasite"]==135){
-    foreach($tersagil3 as $agilsayisi3 => $kapasitesi3){
-        echo $agilsayisi3.": ".$kapasitesi3."<br>";
+for($i=$index[2]["agil"]; $i>=1; $i--){
+    if($index[2]["koyun"] >= $index[2]["agilkapasite"]){
+    echo "$i. Agil: ".$index[2]["agilkapasite"]."<br>";
     }
-    echo "<br>Dışarıda Kalan: $disarida_kalan";
-}
-else{
-    echo "ÇIKTI HATASI";
+};
+
+$kalankoyun = $index[2]["koyun"] - $index[2]["kapasite"];
+
+if($kalankoyun > 0){
+    echo "<br> Kalan Koyun: $kalankoyun";
 }
 
-/*Tamamlamış olduğum bu uygulamada Ağıl sayısı, Ağıl kapasitesi ve Toplam koyun sayısıyla ağıllara koyun yerleştirme uygulaması yaptım.
-Bu uygulamada dizileri, dizi değişkenlerini, If-Else kontrol yapılarını kullandım.
-İlk olarak agil sayısı, agıl kapasitesi ve toplam koyun sayısı için çoklu boyutlu bir dizi oluşturdum.
-If-Else yapsısı ile de ağıl sayısı ile ağıl kapasitesini kontrol ediyorum.
-Agıllara yerleşen koyun sayısını ekrana yazmak içinse farklı bir dizi oluşturdum ve bu diziyi ters çevirip sondan ekrana basa bilmesi için array_reverse() fonksiyonunu kullandım.
-Aynı zamanda dizileri ekrana bastırmak içinse foreach döngüsünü kullandım.*/
+/*Tamamlamış olduğum bu uygulamada Ağıl sayısı, Ağıl kapasitesi, Kapasite ve Toplam koyun sayısıyla ağıllara koyun yerleştirme uygulaması yaptım.
+Bu uygulamada dizileri, for döngülerini, If-Else kontrol yapılarını kullandım.
+İlk olarak ağıl sayısı, ağıl kapasitesi, kapasite ve toplam koyun sayısı için çok boyutlu bir dizi oluşturdum.
+Daha sonra for döngüleri ile ağılları tersten ekrana yazdırdım. For döngüleri içinde if-else ile ağıl kapasitelerini ağıl karşılıklarına yazdırdım
+ve boş ağıl kalırsa kapasite karşılığına sıfır(0) yazdırmasını istedim. For döngüsünün sonunda kalan koyunu kontrol etmek için koyun sayısından ağıl kapasitesini çıkarıp
+yeni değeri tekrar koyuna atadım. Son olarak kalan koyun varsa koyun sayısından kapasiteyi çıkarıp onu $kalankoyuna atadım ve kalan koyun olursa bunu da ekrana yazdırmasını istedim.*/
+
+/* Hocam dersten sonra projede birkaç eksiğim ve yanlışım olduğunu gördüm. O yüzden tekrar güncellemek istedim projeyi ve yanlış yaptığım kısımları düzelttim.*/
 ?>
