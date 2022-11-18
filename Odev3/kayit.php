4c3093ac43452638b1796db642172e79c263dddf
Şeyma Bozan <101289239+seymabozan@users.noreply.github.com>
2022-11-18 13:22:37 +0300

    Update kayit.php

--- Odev3/kayit.php
+++ Odev3/kayit.php
@@ -1,20 +1,20 @@
 <?php
-$baglanti = new PDO("mysql:host=localhost;dbname=odev", "root", "");
+$baglanti = new PDO("mysql:host=localhost;dbname=odev", "root", ""); //New PDO ile veritabanı ile bağlantı kurdum.
 $baglanti->exec("SET NAMES utf8");
 $baglanti->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$adsoyad = $_POST["adsoyad"];
+$adsoyad = $_POST["adsoyad"]; //Form'da name olarak verdiğim isimleri burada bir değişkene atıyorum
 $telefon = $_POST["telefon"];
 
-$sorgu = $baglanti->prepare("INSERT INTO kisiler(adsoyad, telefon) VALUES(?, ?)");
-$sorgu->bindParam(1, $adsoyad, PDO::PARAM_STR);
+$sorgu = $baglanti->prepare("INSERT INTO kisiler(adsoyad, telefon) VALUES(?, ?)"); //Prepare ile verileri hangi tabloya ve değerlere atayacağını/ekleyeceğini belirtiyorum.
+$sorgu->bindParam(1, $adsoyad, PDO::PARAM_STR); //bindParam ile eklenicek verileri tanımlıyoruz.
 $sorgu->bindParam(2, $telefon, PDO::PARAM_STR);
 
 $sorgu->execute();
 
-echo "<script>
-alert('Bilgileriniz Kayıt Edildi!');
+echo "<script> 
+alert('Bilgileriniz Kayıt Edildi!'); 
 window.location.href = 'liste.php';
-</script>";
+</script>"; //Som olarak script ile kayıt yapıldıysa ekrana mesaj yazdırıyoruz.
 
 ?>
